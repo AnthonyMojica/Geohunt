a89bc2a2a35e97a7b25bd8833687ae295ced3284
Anthony <Mojica@fullsail.edu>
2015-11-14 10:49:35 -0500

    All the things

---
+++ MyLibrary.h
@@ -0,0 +1,23 @@
+/*
+ * MyLibrary.h
+ *
+ * Created: 11/14/2015 10:35:49 AM
+ *  Author: Anthony
+ */ 
+
+
+#ifndef MYLIBRARY_H_
+#define MYLIBRARY_H_
+
+float CalculateDistance(float Long, float Lat, float EndLong, float EndLat)
+{
+	
+	
+	
+	
+	
+	
+}
+
+
+#endif /* MYLIBRARY_H_ */
---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+ * GeoHunt.cpp
+ *
+ * Created: 10/31/2015 11:25:12 AM
+ * Author : Anthony
+ */ 
+
+//#include <Arduino.h>
+//
+//
+//int main(void)
+//{
+	//
+	//init();
+    ///* Replace with your application code */
+    //while (1) 
+    //{
+		//
+		//
+		//if(serialEventRun)serialEventRun();
+    //}
+//}
+
